0936b8e73351b3b4a41bf513384cb42009bf9dad
akshitagarg275 <akshitagarg275@gmail.com>
2022-09-24 08:43:26 +0530

    day 2: keywords, variables, constants

---
+++ 01basics/02keywords.py
@@ -0,0 +1,12 @@
+# we are learning about keywords
+# print("Hey")
+'''
+It is multiline
+comment
+'''
+"""
+keywords: these are reserved words which have a specific meaning
+"""
+import keyword
+
+print(keyword.kwlist)
---
+++ 01basics/03variables_constants.py
@@ -0,0 +1,27 @@
+# Variables
+'''
+These are the containers which allow us to store data in the memory
+which can be used again and again
+
+
+
+'''
+
+
+# age = 23
+# print("age is: ",age)
+# print("age")
+
+# age  = "hello"
+# print("age is: ",age)
+
+# Constants: whose value remains unchanged
+'''
+To make a constant, write identifier name all in caps
+'''
+PI = 3.14
+# print(PI)
+
+# PI =7
+# print(PI)
+ 
