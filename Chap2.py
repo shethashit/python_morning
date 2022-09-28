21c5efe2ebc1dddddfa0da7cc6f4aae6bfdab33a
shethashit <113160682+shethashit@users.noreply.github.com>
2022-09-27 22:53:07 -0500

    Add files via upload

---
+++ Chap2.py
@@ -0,0 +1,24 @@
+print("Hello World")
+
+num1 = int(input("Please enter number 1 : "))
+num2 = int(input("Please enter number 2 : "))
+print("The Sum of Number 1 and 2 is: ", num1+num2)
+
+print("The Type for Num1 is ", type(num1))
+
+if num1 % 2 == 0:
+    print("Num1 is Divisible by 2 and the Reminder is ", num1/2)
+else:
+    print("Num1 is not Divisible by 2")
+
+a = 34
+b = 80
+
+if a > b:
+    print(a, "is greater than", b)
+else:
+    print(a, "is not greater than", b)
+
+print("The Average of Num1 and Num2 is ", (num1+num2)/2)
+
+print("the Square of Num1 is ", num1*2)
